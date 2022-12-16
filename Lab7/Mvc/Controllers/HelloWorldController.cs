a0075f16bce11f3424307dd09ea33f534c4abe8b
Lino Tadros <thetrainingboss@gmail.com>
2022-12-16 15:47:21 -0500

    Lab 7

---
+++ Lab7/Mvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SFDev2022.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name ="HelloWorld", Title ="Hello World", SectionName ="SFDev2022")]
+    public class HelloWorldController : Controller
+    {
+        public string Message { get; set; }
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
